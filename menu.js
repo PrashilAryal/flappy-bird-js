dc2d0e19d745f68ac641e0cebfc6dde0a437f946
PrashilAryal <prashilaryal13@gmail.com>
2023-12-07 15:37:00 +0545

    sound controls added

--- menu.js
+++ menu.js
@@ -8,11 +8,19 @@ const startButton = document.getElementById("startButton");
 const settingsButton = document.getElementById("settingsButton");
 const controlsButton = document.getElementById("controlsButton");
 
+const bgSoundButton = document.getElementById("bgSoundButton");
+const collisionSoundButton = document.getElementById("collisionSoundButton");
+const wingsSoundButton = document.getElementById("wingsSoundButton");
+
 const settingsOkButton = document.getElementById("settingsOkButton");
 const controlsOkButton = document.getElementById("controlsOkButton");
 
 const homeButton = document.getElementById("homeButton");
 const restartButton = document.getElementById("restartButton");
+
+let isBgSoundPlay = true;
+let isCollisionSoundPlay = true;
+let isWingsSoundPlay = true;
 
 // menuModal.style.display = "none";
 menuSettingsModal.style.display = "none";
@@ -70,4 +78,45 @@ restartButton.addEventListener("click", () => {
   gameOverModal.style.display = "none";
   init();
   animate();
+});
+
+bgSoundButton.addEventListener("click", () => {
+  if (isBgSoundPlay) {
+    isBgSoundPlay = false;
+    bgSoundButton.classList.remove("btn-on");
+    bgSoundButton.classList.add("btn-off");
+    bgSoundButton.innerHTML = "Off";
+  } else {
+    isBgSoundPlay = true;
+    bgSoundButton.classList.remove("btn-off");
+    bgSoundButton.classList.add("btn-on");
+    bgSoundButton.innerHTML = "On";
+  }
+});
+collisionSoundButton.addEventListener("click", () => {
+  if (isCollisionSoundPlay) {
+    isCollisionSoundPlay = false;
+    collisionSoundButton.classList.remove("btn-on");
+    collisionSoundButton.classList.add("btn-off");
+    collisionSoundButton.innerHTML = "Off";
+  } else {
+    isCollisionSoundPlay = true;
+    collisionSoundButton.classList.remove("btn-off");
+    collisionSoundButton.classList.add("btn-on");
+    collisionSoundButton.innerHTML = "On";
+  }
+});
+
+wingsSoundButton.addEventListener("click", () => {
+  if (isWingsSoundPlay) {
+    isWingsSoundPlay = false;
+    wingsSoundButton.classList.remove("btn-on");
+    wingsSoundButton.classList.add("btn-off");
+    wingsSoundButton.innerHTML = "Off";
+  } else {
+    isWingsSoundPlay = true;
+    wingsSoundButton.classList.remove("btn-off");
+    wingsSoundButton.classList.add("btn-on");
+    wingsSoundButton.innerHTML = "On";
+  }
 });
--- sound.js
+++ sound.js
@@ -5,16 +5,22 @@ const collisionSound = new Audio();
 collisionSound.src = "./sounds/collision.mp3";
 
 function playBackgroundSound() {
-  bgSound.src = "./sounds/background.mp3";
-  bgSound.volume = 0.2;
-  bgSound.play();
+  if (isBgSoundPlay) {
+    bgSound.src = "./sounds/background.mp3";
+    bgSound.volume = 0.2;
+    bgSound.play();
+  }
 }
 function playWingSound() {
-  wingSound.src = "./sounds/wings.mp3";
-  wingSound.volume = 0.2;
-  wingSound.play();
+  if (isWingsSoundPlay) {
+    wingSound.src = "./sounds/wings.mp3";
+    wingSound.volume = 0.2;
+    wingSound.play();
+  }
 }
 function playCollisionSound() {
-  wingSound.volume = 0.1;
-  collisionSound.play();
+  if (isCollisionSoundPlay) {
+    wingSound.volume = 0.1;
+    collisionSound.play();
+  }
 }
